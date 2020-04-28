379f721366bec7f4dc3eba6e9f4a950ee8d85958
Franco Solerio <franco@solerio.net>
2020-04-28 12:40:35 +0200

    Apply environment variables with function

--- start.sh
+++ start.sh
@@ -4,43 +4,26 @@ env
 
 set -x
 
+set_val() {
+    if [ -n "$2" ]; then
+        echo "set '$2' to '$1'"
+        sed -i "s/<$2>[^<]*<\/$2>/<$2>$1<\/$2>/g" /etc/icecast-owned.xml
+    else
+        echo "WARNING: setting for '$1' is missing!" >&2
+	fi
+}
+
 cp /etc/icecast.xml /etc/icecast-owned.xml
 
-if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i'' "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
-    sed -i'' "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
-    sed -i'' "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_PASSWORD" ]; then
-    sed -i'' "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_LOCATION" ]; then
-    sed -i'' "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_HOSTNAME" ]; then
-    sed -i'' "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_ADMIN" ]; then
-    sed -i'' "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN<\/admin>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_MAX_SOURCES" ]; then
-    sed -i'' "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast-owned.xml
-fi
-
-if [ -n "$ICECAST_MAX_LISTENERS" ]; then
-    sed -i'' "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_LISTENERS<\/clients>/g" /etc/icecast-owned.xml
-fi
+set_val $ICECAST_SOURCE_PASSWORD 	source-password
+set_val $ICECAST_RELAY_PASSWORD 	relay-password
+set_val $ICECAST_ADMIN_PASSWORD 	admin-password
+set_val $ICECAST_PASSWORD 			password
+set_val $ICECAST_LOCATION 			location
+set_val $ICECAST_HOSTNAME 			hostname
+set_val $ICECAST_ADMIN 				admin
+set_val $ICECAST_MAX_SOURCES 		sources
+set_val $ICECAST_MAX_LISTENERS 		clients
 
 sed -i'' "s/<ssl-private-key>[^<]*<\/ssl-private-key>/<ssl-private-key>\/etc\/key-owned.pem<\/ssl-private-key>/g" /etc/icecast-owned.xml
 
